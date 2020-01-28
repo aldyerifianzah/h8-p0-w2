ae97bf6d117f8fe031b8451e16010b530128dc38
aldyerifianzah <aldyerifianzah.edu@gmail.com>
2020-01-29 04:07:28 +0700

    penambahan revisi tugas1

--- tugas1revisi.js
+++ tugas1revisi.js
@@ -1,28 +1,26 @@
-//deklarasi 
-
 const kalimatTabit = 'kamu akan membantu temanmu yang terluka';
 const KalimatKsatria = 'kamu dapat menyerang dengan senjatamu!';
 const KalimatPenyihir = 'ciptakan keajaiban yang membantu kemenanganmu!';
-let dasar =' nama harus di isi! dan peran harus di isi!'; 
+
+//input
+var nama ='Lauv';
+var peran ='Penyihir';
 
 
-
-
-
-//ini input kosong
-
-var nama = 'Neke';
-var peran = 'Ksatria';
- if(nama ==='Aldy' && peran === ''){
+if(nama==='Aldy' && peran ===''){
     console.log(`halo ${nama}, pilih peranmu untuk memulai game! `);
-}else if(nama=== 'Neke' && peran=== 'Ksatria'){
+
+}else if(nama==='Neke' && peran==='Ksatria'){
     console.log(`selamat datang di Dunia Proxytia, ${nama}`);
     console.log(`Halo ${peran} ${nama},` + KalimatKsatria);
-}else if(nama=== 'Lany' && peran=== 'Tabib'){
+
+}else if(nama==='Lany' && peran==='Tabib'){
     console.log(`selamat datang di Dunia Proxytia, ${nama}`);
     console.log(`Halo ${peran} ${nama},` + kalimatTabit);
-   }else if(nama=== 'Lauv' && peran=== 'Penyihir'){
+
+}else if(nama==='Lauv' && peran==='Penyihir'){
     console.log(`selamat datang di Dunia Proxytia, ${nama}`);
-    console.log(`Halo ${peran} ${nama},` + KalimatPenyihir);    
-}else (nama==='' && peran===''){
-    console.log("Nama harus diisi!");}
+    console.log(`Halo ${peran} ${nama},` + KalimatPenyihir);
+}else{
+    console.log('nama harus di isi!');
+}
